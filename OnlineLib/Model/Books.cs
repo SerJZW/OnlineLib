e44bf9d71484e996cc6ba18b7a1c0d4243f830f1
zemzh <zemzh@DESKTOP-BLPL8VP>
2023-10-15 00:09:39 +0700

    MVVM

---
+++ OnlineLib/Model/Books.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineLib
+{
+    public class Books : Notice
+    {
+        private string? autor;
+        private short acr;
+        private DateOnly age;
+        private int count;
+        public string? Autor
+        {
+            get { return autor; }
+            set { autor = value; OnPropertyChanged("Autor"); }
+        }
+        public short Acr
+        {
+            get { return acr; }
+            set { acr = value; OnPropertyChanged("Acr"); }
+        }
+        public DateOnly Age
+        {
+            get { return age; }
+            set { age = value; OnPropertyChanged("Age"); }
+        }
+        public int Count
+        {
+            get { return count; }
+            set { count = value; OnPropertyChanged("Count"); }
+        }
+    }
+}
---
+++ OnlineLib/ModelView/UsersVM.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace OnlineLib.ModelView
+{
+    class UsersVM : Notice
+    {
+        private Users users;
+        UsersVM(Users u) 
+        {
+            users = u;
+        }
+        public int ID
+        {
+            get { return users.ID; }
+            set { users.ID = value; OnPropertyChanged("ID"); }
+        }
+        public string? Name
+        {
+            get { return users.Name; }
+            set { users.Name = value; OnPropertyChanged("Name"); }
+        }
+        public string? Family
+        {
+            get { return users.Family; }
+            set { users.Family = value; OnPropertyChanged("Family"); }
+        }
+        public List<Books>? UserBooks
+        {
+            get { return users.UserBooks; }
+            set { users.UserBooks = value; OnPropertyChanged("UserBooks"); }
+        }
+
+    }
+}

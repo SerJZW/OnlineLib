53491b23cb103c97f7b6b914649865abdbf35ca9
zemzh <zemzh@DESKTOP-BLPL8VP>
2023-10-15 00:11:26 +0700

    Revert "Revert "MVVM""
    This reverts commit c0dfde7118bb6b2c53c9dbbbcb965c792c22ceb4.

---
+++ OnlineLib/Model/Users.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineLib
+{
+    public class Users : Notice
+    {
+        private int id;
+        private string? name;
+        private string? family;
+        private List<Books>? userBooks;
+
+        public int ID
+        {
+            get { return id; }
+            set { id = value; OnPropertyChanged("ID"); } }
+        public string? Name
+        {
+            get { return name; }
+            set { name = value; OnPropertyChanged("Name"); }
+        }
+        public string? Family
+        {
+            get { return family; }
+            set { family = value; OnPropertyChanged("Family"); }
+        }
+        public List<Books>? UserBooks
+        {
+            get { return userBooks; }
+            set { userBooks = value; OnPropertyChanged("UserBooks"); }
+        }
+    }
+}
+    
+
---
+++ OnlineLib/ModelView/BooksVM.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace OnlineLib.ModelView
+{
+    class BooksVM : Notice
+    {
+        private Books books;
+
+        public BooksVM(Books b)
+        {
+            books = b;
+        }
+        public string? Autor
+        {
+            get { return books.Autor; }
+            set { books.Autor = value; OnPropertyChanged("Autor"); }
+        }
+        public short Acr
+        {
+            get { return books.Acr; }
+            set { books.Acr = value; OnPropertyChanged("Acr"); }
+        }
+        public DateOnly Age
+        {
+            get { return books.Age; }
+            set { books.Age = value; OnPropertyChanged("Age"); }
+        }
+        public int Count
+        {
+            get { return books.Count; }
+            set { books.Count = value; OnPropertyChanged("Count"); }
+        }
+    }
+}
